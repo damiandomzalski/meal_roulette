6928c4b0d3d3b5588a1af21e1eb048577b6aec08
Damian Domzalski <damian.domzalski@lano.io>
2023-11-06 05:19:26 +0100

    Upcoming scope changed to look only to upcoming days, not today

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -8,7 +8,7 @@ class MealPlan < ApplicationRecord
   validates :meal_type, presence: true, inclusion: { in: MEAL_TYPES }
   validates_uniqueness_of :meal_type, scope: [:user_id, :date]
 
-  scope :upcoming, -> { where("date >= ?", Date.today) }
+  scope :upcoming, -> { where("date > ?", Date.today) }
   scope :missing_meal_types, lambda { |user, date|
     where(user: user, date: date).pluck(:meal_type).then { |types| MEAL_TYPES - types }
   }
